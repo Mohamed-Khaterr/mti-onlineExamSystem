03a8804ff752184a26a4fb7365242c67963cf6ff
Mohamed-Khater <mohamed.15751@gmail.com>
2022-05-18 00:18:32 +0200

    update ui for createUsers

--- app/Views/admin/createUser.php
+++ app/Views/admin/createUser.php
@@ -13,13 +13,13 @@
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
 	<!-- Vendor CSS Files -->
-	<!--<link href="<?= base_url() ?>/module/admin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?= base_url() ?>/module/admin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?= base_url() ?>/module/admin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
 	<link href="<?= base_url() ?>/module/admin/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
 	<link href="<?= base_url() ?>/module/admin/assets/vendor/quill/quill.snow.css" rel="stylesheet">
 	<link href="<?= base_url() ?>/module/admin/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
 	<link href="<?= base_url() ?>/module/admin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-	<link href="<?= base_url() ?>/module/admin/assets/vendor/simple-datatables/style.css" rel="stylesheet">-->
+	<link href="<?= base_url() ?>/module/admin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
 	
 	<link rel="stylesheet" href="<?= base_url() ?>/assets/css/popupModel.css">
 
@@ -100,7 +100,7 @@
 		<!-- NAVBAR -->
 		<nav>
 			<i class='bx bx-menu' ></i>
-			<a href="#" class="nav-link">Categories</a>
+			<a href="" class="nav-link"><h3>Admin</h3></a>
 			<form action="#">
 				<div class="form-input">
 					
@@ -500,8 +500,8 @@ function stopPopupModel(){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 	
 
-	<script src="script.js"></script>
-    <script src="js.js"></script>
+	<script src="<?= base_url() ?>/module/admin/script.js"></script>
+    <script src="<?= base_url() ?>/module/admin/js.js"></script>
 
 </body>
 </html>
